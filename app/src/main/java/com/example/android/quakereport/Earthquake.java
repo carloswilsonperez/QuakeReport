c60058e0b4142c1dce7ede12fe8adc4d451bcea9
Carlos Alberto Wilson Pérez <carloswilsonperez@gmail.com>
2020-08-30 13:46:31 -0500

    Changing time to long data type

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -3,18 +3,18 @@ package com.example.android.quakereport;
 public class Earthquake {
     private String mMagnitude;
     private String mLocation;
-    private String mDate;
+    private Long mTimeInMilliseconds;
 
     /**
      * Constructs a new {@Link Earthquake} object.
      * @param magnitude is the magnitude of the earthquake
      * @param location is the city location of the earthquake
-     * @param date is the date the earthquake happened
+     * @param time is the date the earthquake happened, in milliseconds
      */
-    public Earthquake(String magnitude, String location, String date) {
+    public Earthquake(String magnitude, String location, long time) {
         mMagnitude = magnitude;
         mLocation = location;
-        mDate = date;
+        mTimeInMilliseconds = time;
     }
 
     public String getMagnitude () {
@@ -25,7 +25,7 @@ public class Earthquake {
         return mLocation;
     }
 
-    public String getDate () {
-        return mDate;
+    public Long getTimeInMilliseconds () {
+        return mTimeInMilliseconds;
     }
 }
